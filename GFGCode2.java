fbf57d229b4a578e489fd3fd0274fbdedaf961a4
Parvez Alam <parvezimohammed@gmail.com>
2022-06-24 00:44:20 +0530

    Day-1: Value equal to index value

---
+++ GFGCode2.java
@@ -0,0 +1,22 @@
+//Value equal to index value: Day-1
+
+import java.util.ArrayList;
+
+public class GFGCode2 {
+    public static void main(String[] args) {
+        ValueEqualToIndex valueEqualToIndex = new ValueEqualToIndex();
+        System.out.println(valueEqualToIndex.valueEqualToIndex(new int[]{3,2,1,4,6},5));
+    }
+}
+
+class ValueEqualToIndex {
+    ArrayList<Integer> valueEqualToIndex(int[] arr, int n) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == i + 1) {
+                arrayList.add(arr[i]);
+            }
+        }
+        return arrayList;
+        }
+    }
